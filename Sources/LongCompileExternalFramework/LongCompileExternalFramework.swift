6e42270e44dd21374bb23346d0543f0bc1a71912
Chris Martin <chris.martin@eventbrite.com>
2020-09-18 09:32:20 -0500

    Make it take some time to compile

--- Sources/LongCompileExternalFramework/LongCompileExternalFramework.swift
+++ Sources/LongCompileExternalFramework/LongCompileExternalFramework.swift
@@ -1,3 +1,26 @@
+// These combined statements take roughly 1m to type check.
+public let __tmp00 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp01 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp02 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp03 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp04 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp05 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp06 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp07 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp08 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp09 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp10 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp11 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp12 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp13 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp14 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp15 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp16 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp17 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp18 = 2 * 2 * 2 * 2.0 / 2 + 2
+public let __tmp19 = 2 * 2 * 2 * 2.0 / 2 + 2
+
 struct LongCompileExternalFramework {
-    var text = "Hello, World!"
+
+    public let theMeaningOfLife = 42
 }
--- Tests/LongCompileExternalFrameworkTests/LongCompileExternalFrameworkTests.swift
+++ Tests/LongCompileExternalFrameworkTests/LongCompileExternalFrameworkTests.swift
@@ -6,7 +6,7 @@ final class LongCompileExternalFrameworkTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LongCompileExternalFramework().text, "Hello, World!")
+        XCTAssertEqual(LongCompileExternalFramework().theMeaningOfLife, 42)
     }
 
     static var allTests = [
